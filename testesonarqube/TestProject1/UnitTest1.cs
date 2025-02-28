d1584376bbcc4f47607837994e7754433952c62a
thauanesx <thauan.santos@esx.com.br>
2025-02-28 17:15:54 -0300

    test

---
+++ testesonarqube/TestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+
+using testesonarqube.Controllers;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        private readonly testController controller=new testController();
+          [Fact]
+        public void Test1()
+        {
+            var a = controller.Index();
+            Assert.NotNull(a);
+        }
+    }
+}
---
+++ testesonarqube/testesonarqube/Controllers/testController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testesonarqube.Controllers
+{
+    public class testController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok('a');
+        }
+    }
+}
