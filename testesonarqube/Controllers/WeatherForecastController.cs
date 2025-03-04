2bbb66128ef4cb3a45c14353dfca21e1f2d1d4b8
Thauanesx <thauan.santos@esx.com.br>
2025-03-04 19:51:12 -0300

    Update WeatherForecastController.cs

--- testesonarqube/Controllers/WeatherForecastController.cs
+++ testesonarqube/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ namespace testesonarqube.Controllers
     [ApiController]
     [Route("[controller]")] 
     public class WeatherForecastController : ControllerBase
-    {
+    { 
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
