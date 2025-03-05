df6e0e36f23c947b7429fb808fd350ede4a9d67e
Thauanesx <thauan.santos@esx.com.br>
2025-03-05 14:38:39 -0300

    Update UnitTest1.cs

--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -4,7 +4,7 @@ using testesonarqube.Controllers;
 
 namespace TestProject2
 {  
-    public class UnitTest1
+    public class UnitTest1 
     {    
         private readonly testController controller;
 
