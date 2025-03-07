5ad74c028cce312353de0134d58ac9afdbd3cab1
Thauanesx <thauan.santos@esx.com.br>
2025-03-07 09:32:43 -0300

    Update WeatherForecast.cs

--- testesonarqube/WeatherForecast.cs
+++ testesonarqube/WeatherForecast.cs
@@ -2,7 +2,7 @@ namespace testesonarqube
 {
     public class WeatherForecast
     {
-        public DateOnly Date { get; set; }
+        public DateOnly Date { get; set; } 
 
         public int TemperatureC { get; set; }
 
